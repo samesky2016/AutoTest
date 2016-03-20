0c28a57a5bad7b8270da6f63505950bc0005cb0b
samesky2016 <84028058@qq.com>
2016-03-20 22:30:55 +0800

    my first project

---
+++ RMITest/src/com/rmi/test/TestDemo1.java
@@ -0,0 +1,10 @@
+package com.rmi.test;
+
+public class TestDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first github project~!");
+	}
+
+}
